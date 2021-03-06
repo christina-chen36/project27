6ad982bf37a1d5b75bd9e5390379128b2ae52fb9
christina-chen36 <76858808+christina-chen36@users.noreply.github.com>
2021-03-06 18:02:57 +1100

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,23 @@
+class Bob {
+  constructor(x, y, radius) {
+    var options = {
+      restitution: 0,
+      friction: 0.5,
+    };
+    this.body = Bodies.circle(x, y, radius, options);
+    this.radius = radius;
+
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    //rectMode(CENTER);
+    fill("purple");
+    circle(0, 0, this.radius * 2);
+    pop();
+  }
+}
